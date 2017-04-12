7df5e80affe8c2ff2a94d789dc9c3aabb2a9f429
Piero Giusti <pierophp@gmail.com>
2017-04-12 16:53:51 -0300

    Fix generator

--- src/Core/Generator.php
+++ src/Core/Generator.php
@@ -2,71 +2,89 @@
 
 namespace LaravelSwaggerGenerator\Core;
 
-use Doctrine\Common\Annotations\AnnotationReader;
-use Doctrine\Common\Annotations\AnnotationRegistry;
-use LaravelSwaggerGenerator\Mapping\Get;
-
 class Generator
 {
-    public function generate()
-    {
-        new Get([]);
+    protected $route;
 
+    protected $reflectionMethod;
+
+    protected $description;
+
+    public function generate()
+
+    {
         $api = app('Dingo\Api\Routing\Router');
         $routes = $api->getRoutes();
-        $annotationReader = new AnnotationReader();
-
         $i = 0;
         foreach($routes['v1'] as $dingoRoute) {
-            $route = $dingoRoute->getOriginalRoute();
+            $this->route = $dingoRoute->getOriginalRoute();
             $i++;
 
-            $classExplode = explode('@', $route->action['controller']);
+            $classExplode = explode('@', $this->route->action['uses']);
             $className = $classExplode[0];
             $methodName = $classExplode[1];
+            
             $reflectionClass = new \ReflectionClass($className);
-            $reflectionMethod = $reflectionClass->getMethod($methodName);
+            $this->reflectionMethod = $reflectionClass->getMethod($methodName);
             $filename = $reflectionClass->getFileName();
             
-            $originalComment = $reflectionMethod->getDocComment();;
-            $newComment = $this->parseComment($route, $originalComment);
-
-            dump($filename);
-            dd($annotationReader->getMethodAnnotations($reflectionMethod));
+            $originalComment = $this->reflectionMethod->getDocComment();;
+            $newComment = $this->parseComment($originalComment);
 
             $fileContent = file_get_contents($filename);
             $fileContent = str_replace($originalComment, $newComment, $fileContent);
 
             // file_put_contents($filename, $fileContent);
-
-
-
-
-            exit;
-            dump($route->uri);
-            dump($route->methods);
-            dump($route->controller);
-            dump($route->action);
-            dump($route->parameters);
-            dump($route->parameterNames);
-
             
-            if ($i === 1) {
+            if ($i === 10) {
                 exit;
             }
         }
     }
 
-    protected function parseComment($route, $originalComment) 
+    protected function parseComment($originalComment) 
     {
         $newComment = '';
         $lines = explode("\n", $originalComment);   
-        $i = 0;     
+        $i = 0;
+        $docComments = '';
+        $returnComments = '';
+        $docCommentsOpen = false;
+        $totalParenthesis = 0;
+
+        $this->description = '';
+
         foreach($lines as $line) {
             $i++;
-            if ($i == 2) {
-                $newComment .= '     * TESTE' . "\n";    
+
+            $parsedLine = trim($line, ' */');
+            $parseStartString = '@SWG\\';
+
+            if (substr($parsedLine, 0, strlen($parseStartString)) === $parseStartString) {
+                $docCommentsOpen = true;
             }
+
+            $parseStartString = '@return';
+            if (substr($parsedLine, 0, strlen($parseStartString)) === $parseStartString) {
+                $returnComments = $line;
+            }
+
+            $parseStartString = '@';
+            if (substr($parsedLine, 0, strlen($parseStartString)) !== $parseStartString && !$this->description) {
+                $this->description = $parsedLine;
+            }
+
+            if ($docCommentsOpen) {
+                $docComments .= $line . "\n";
+                $startParenthesis = substr_count($parsedLine, '(');
+                $endParenthesis = substr_count($parsedLine, ')');
+                $totalParenthesis += $startParenthesis;
+                $totalParenthesis -= $endParenthesis;
+                if ($totalParenthesis === 0) {
+                    $docCommentsOpen = false;
+                }
+            }
+
             $newComment .= $line;
 
             if($i < count($lines)){
@@ -74,6 +92,72 @@ class Generator
             }
         }
 
+        $docComments = trim($docComments, "\n");
+        $newDocComments = $this->generateComment();
+
+        if ($docComments) {
+            $newComment = str_replace($docComments, $newDocComments, $newComment);
+        } else if ($returnComments){
+            $newComment = str_replace($returnComments, $newDocComments . "\n" . $returnComments, $newComment);
+        } else {
+
+        }
+        
+
+        dump($newComment);
+
         return $newComment;
+    }
+
+    protected function generateComment()
+    {
+        $operationId = '';
+        if (!empty($this->route->action['as'])) {
+            $operationId = $this->route->action['as'];
+        }
+        
+        $params = [
+            'path' => $this->route->uri,
+            'summary' => $this->description,
+            'description' => $this->description,
+            'operationId' => $operationId,
+            'consumes' => '{"application/json"}',
+            'produces' => '{"application/json"}',
+        ];
+
+        $method = '\\' . ucfirst(strtolower($this->route->methods[0]));
+        $comments = '';
+        $comments .= "     * @SWG{$method}(' . \n";
+        foreach ($params as $key => $val){
+            $comments .= "     *     {$key}=\"{$val}\",\n";
+        } 
+
+        foreach ($this->reflectionMethod->getParameters() as $parameter) {
+            if ($parameter->getType() && in_array($parameter->getType(), [
+                'Illuminate\Http\Request',
+            ])) {
+                continue;
+            }
+
+            $required = 'true';
+            if ($parameter->isOptional()) {
+                $required = 'false';
+            }
+            $comments .= "     *     @SWG\Parameter(\n";
+            $comments .= "     *         in=\"path\",\n";
+            $comments .= "     *         name=\"{$parameter->name}\",\n";
+            $comments .= "     *         description=\"\",\n";
+            if ($parameter->getType()) {
+                $comments .= "     *         type=\"{$parameter->getType()}\",\n";
+            }
+       
+            $comments .= "     *         required={$required}\n";
+            $comments .= "     *     ),\n";
+        }
+
+
+        $comments = trim($comments, ",\n") . "\n";
+        $comments .= '     * )';
+        return $comments;
     }
 }
